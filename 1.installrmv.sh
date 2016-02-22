8c5e66dfdac17773ce3d86d0e3e50d78a47c647f
Serge Laviolette <slaviole@ciena.com>
2016-02-22 20:22:07 +0000

    rename scripts files

--- 1.installrmv.sh
+++ 1.installrmv.sh
@@ -1,6 +1,5 @@
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 sudo apt-get install rvm
-source /etc/profile.d/rvm.sh
 
 # issue "source /etc/profile.d/rvm.sh" after running script
